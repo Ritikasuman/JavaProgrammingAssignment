4c3ff126763ff8e14c509545a7c68cfb801c2da1
Ritika Suman <142476227+Ritikasuman@users.noreply.github.com>
2024-09-12 15:49:09 +0530

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,24 @@
+/**
+ * Defines constants used for prompts, messages, and options in the main program.
+ * Provides standard messages and options to maintain consistency across the application.
+ */
+public class Constants {
+    public static final String INVALID_INPUT_MESSAGE = "Invalid input. Please enter a number.";
+    public static final String INVALID_NUMBER_FORMAT_MESSAGE = "Invalid number format.";
+    public static final String INVALID_OPTION_MESSAGE = "Invalid";
+    public static final String INVALID_CHOICE_MESSAGE = "Invalid";
+    public static final String CHOOSE_OPTION_PROMPT = "Choose an option:";
+    public static final String BINARY_STRING_PROMPT = "Enter a binary string:";
+    public static final String NTH_NUMBER_PROMPT = "Enter the Nth number:";
+    public static final String STRING_PROMPT = "Enter a string:";
+    public static final String PALINDROME_COUNT_PROMPT = "The number of palindromes are:";
+    public static final String OPTION_BINARY_TO_DECIMAL = "1. Binary to Decimal";
+    public static final String OPTION_NTH_FIBONACCI = "2. Nth Fibonacci Number";
+    public static final String OPTION_COUNT_CONSONANTS = "3. Count Consonants in a String";
+    public static final String OPTION_CONVERT_SNAKE_TO_CAMEL = "4. Convert Snake case to Camel case";
+    public static final String OPTION_COUNT_PALINDROMES = "5. Count palindromes in a String";
+    public static final String CONSONANT_COUNT_MESSAGE = "Number of consonants: ";
+    public static final String DECIMAL_VALUE_MESSAGE = "Decimal Value: ";
+    public static final String FIBONACCI_NUMBER_MESSAGE = "Fibonacci Number: ";
+    public static final String CONVERTED_OUTPUT_MESSAGE = "Output: ";
+}
